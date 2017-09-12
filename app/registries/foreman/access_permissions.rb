ab2462f936829c751afa2c2e1062d1cbbf539e69
dhlavac <dhlavacd@redhat.com>
2017-09-12 23:10:52 +0200

    Fixes #15286 - New API for auth sources

--- app/registries/foreman/access_permissions.rb
+++ app/registries/foreman/access_permissions.rb
@@ -33,7 +33,10 @@ Foreman::AccessControl.map do |permission_set|
     ajax_actions = [:test_connection]
     map.permission :view_authenticators, {:auth_source_ldaps => [:index, :show],
                                              :"api/v1/auth_source_ldaps" => [:index, :show],
-                                             :"api/v2/auth_source_ldaps" => [:index, :show]
+                                             :"api/v2/auth_source_ldaps" => [:index, :show],
+                                             :"api/v2/auth_sources" => [:index, :show],
+                                             :"api/v2/auth_source_internals" => [:index, :show],
+                                             :"api/v2/auth_source_externals" => [:index, :show]
     }
     map.permission :create_authenticators, {:auth_source_ldaps => [:new, :create].push(*ajax_actions),
                                              :"api/v1/auth_source_ldaps" => [:create],
@@ -41,7 +44,8 @@ Foreman::AccessControl.map do |permission_set|
     }
     map.permission :edit_authenticators, {:auth_source_ldaps => [:edit, :update].push(*ajax_actions),
                                              :"api/v1/auth_source_ldaps" => [:update],
-                                             :"api/v2/auth_source_ldaps" => [:update, :test]
+                                             :"api/v2/auth_source_ldaps" => [:update, :test],
+                                             :"api/v2/auth_source_externals" => [:update]
     }
     map.permission :destroy_authenticators, {:auth_source_ldaps => [:destroy],
                                              :"api/v1/auth_source_ldaps" => [:destroy],
